c3ef8802e826e89bbd8b488b54c1189d2dc0a83e
noramanina2014842606 <ieyna910@gmail.com>
2015-10-22 20:13:25 +0800

    Company-A-IPV6

---
+++ Test2-Java/CompanyA.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import static java.lang.System.out;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import static java.lang.System.out;
+
+public class CompanyA {
+
+public static void main(String args[]) throws SocketException {
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
+    }
+
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+       
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+        out.printf("\n");
+     }
+}  
